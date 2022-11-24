195b8b3aab7f893fa557aa75790f4eb22e737ac8
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-24 10:53:31 +0600

    firebase added

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,76 @@
+import React, { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [loadingState, setLoadingState] = useState(true);
+  const [user, setUser] = useState(null);
+  // registration starting
+  const createUserEmail = (email, password) => {
+    setLoadingState(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // registration ending
+  // update starting
+  const updateUser = (info) => {
+    setLoadingState(true)
+    return updateProfile(auth.currentUser, info)
+  }
+  // update ending
+  // google login starting
+  const googleLogin = () => {
+    setLoadingState(true)
+    return signInWithPopup(auth, googleProvider)
+  }
+  // google login ending
+  // login starting
+  const emailLogin = (email, password) => {
+    setLoadingState(true)
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  // login ending
+  // logout starting
+  const logOut = () => {
+    return signOut(auth)
+  }
+  // logout ending
+  // get currently signed in user
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoadingState(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    loadingState,
+    setLoadingState,
+    user,
+    setUser,
+    createUserEmail,
+    updateUser,
+    googleLogin,
+    emailLogin,
+    logOut
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
