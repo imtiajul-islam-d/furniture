8033cd31de1868fe757c65803068d97d41bcfbb2
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-24 08:03:57 +0600

    navbar added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home/Home";
 import Error from "../../Pages/Shared/Error/Error";
 
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
     }
